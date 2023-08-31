ea2cebaddee9f6f15324d346e367805a8dbcd6f6
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-08-31 11:38:15 +0900

    リソースベースルーティングを使う

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "boards" => "boards#index"
-  get "boards/new" => "boards#new"
-  post "boards" => "boards#create"
-  get "boards/:id" => "boards#show"
+  resources :boards, only: [:index, :new, :create, :show]
 end
