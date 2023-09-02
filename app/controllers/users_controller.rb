ae9e8e0b490f31a6c6fa4724b923aeaff88f9f83
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-09-03 00:28:50 +0900

    認証機能を実装する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User.new(flash[:user])
   end
 
   def create
+    user = User.new(users_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      flash[:error_messages] = user.errors.full_messages
+      flash[:user] = user
+      redirect_back fallback_location: new_user_path
+    end
   end
 
   def me
+    
+    binding.pry
+    
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
