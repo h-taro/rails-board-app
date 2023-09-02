d3481a4cbf6038691e734f4ad4497ccb129a9795
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-09-02 15:11:01 +0900

    認証に必要なファイルを作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  get 'session/create'
+  get 'session/destroy'
   root 'home#index'
   resources :boards
   resources :comments, only: [:create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_02_042330) do
+ActiveRecord::Schema.define(version: 2023_09_02_060812) do
 
   create_table "board_tag_relations", force: :cascade do |t|
     t.integer "board_id", null: false
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2023_09_02_042330) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "board_tag_relations", "boards"
