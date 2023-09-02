2ef8a6b8020e971bae544e876b722e0fc86e5176
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-09-02 23:47:53 +0900

    認証情報に関するルーティングを修正する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
-  get 'session/create'
-  get 'session/destroy'
   root 'home#index'
+
+  get 'mypage', to: 'users#me'
+
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
   resources :boards
   resources :comments, only: [:create, :destroy]
 end
