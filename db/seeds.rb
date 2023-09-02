7fa8185a01a2f97b8e898233aed96d186ee4c098
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-09-02 13:43:00 +0900

    タグを表示する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
@@ -8,6 +8,15 @@
 
 if Rails.env == 'development'
   (1..50).each do |i|
-    Board.create(name: "user#{i}", title: "title#{i}", body: "body#{i}")
+    Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
   end
+
+  Tag.create([
+    { name: 'Ruby' },
+    { name: 'Ruby on Rails4' },
+    { name: 'Ruby on Rails5' },
+    { name: 'Python2' },
+    { name: 'Python3' },
+    { name: 'Django2' }
+  ])
 end
