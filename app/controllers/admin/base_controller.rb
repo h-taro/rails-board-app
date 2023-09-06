9236f8d8e937c7f585fb7ca6ec10541e4d1e9376
Hiraishi Taro <tarohiraishi1@gmail.com>
2023-09-06 09:55:29 +0900

    admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,15 @@
 class Admin::BaseController < ApplicationController
+  bofore_action :check_admin
+  
   layout 'admin/layouts/application'
+
+  private
+
+  def not_authenticated
+    redirect_to admin_login_path, warning: ''
+  end
+
+  def check_admin
+    redirect_to root_path unless current_user.admin?
+  end
 end
